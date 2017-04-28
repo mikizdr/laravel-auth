4851abb346d857a219a633dd76caa04301932a01
Мирослав Здравковић <miroslav.zdravkovic@gmail.com>
2017-04-28 17:27:41 +0200

    Create migration for admin

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 // admin routes
 Route::get('admin\home', 'AdminController@index');
-Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
-Route::POST('login', 'Admin\LoginController@login');
+Route::get('admin', 'Admin\LoginController@showLoginForm')->name('admin.login');
+Route::POST('admin', 'Admin\LoginController@login');
 Route::POST('admin-password\email', 'Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
 Route::get('admin-password\reset', 'Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
 Route::POST('admin-password/reset', 'Admin\ResetPasswordController@reset');
