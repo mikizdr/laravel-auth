e48971c362d855fbac4d10341761c7aa535ae178
Мирослав Здравковић <miroslav.zdravkovic@gmail.com>
2017-04-28 17:29:26 +0200

    Fixes mistakes in admin routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,10 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 // admin routes
-Route::get('admin\home', 'AdminController@index');
+Route::get('admin/home', 'AdminController@index');
 Route::get('admin', 'Admin\LoginController@showLoginForm')->name('admin.login');
 Route::POST('admin', 'Admin\LoginController@login');
-Route::POST('admin-password\email', 'Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
-Route::get('admin-password\reset', 'Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+Route::POST('admin-password/email', 'Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+Route::get('admin-password/reset', 'Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
 Route::POST('admin-password/reset', 'Admin\ResetPasswordController@reset');
 Route::get('admin-password/reset/{token}', 'Admin\ResetPasswordController@showResetForm')->name('admin.password.reset');
