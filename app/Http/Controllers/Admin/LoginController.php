86ffef68a64ca81e197990294a62bfd2fcefbe44
Raganor <miroslav.zdravkovic@gmail.com>
2017-04-29 20:27:00 +0200

    Show admin login page

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -36,4 +36,14 @@ class LoginController extends Controller
     {
         $this->middleware('guest:admin')->except('logout');
     }
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('admin.login');
+    }
 }
