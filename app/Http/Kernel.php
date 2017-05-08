43a6e50d43e8875b68c3f46284ff6febb8a8ecf2
mikizdr <miroslav.zdravkovic@gmail.com>
2017-05-08 14:04:48 +0200

    2. Register admin middleware in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'editor' => \App\Http\Middleware\EditorMiddleware::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
