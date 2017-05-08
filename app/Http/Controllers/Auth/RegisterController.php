df6f9f596600c8f4d8c9a4166cbf7cc9e5524aac
mikizdr <miroslav.zdravkovic@gmail.com>
2017-05-08 16:20:36 +0200

    Add verifyToken in User model and Auth/RegisterController (copy Str::random(40) from DatabaseTokenRepository for verifyToken)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -70,6 +71,7 @@ class RegisterController extends Controller
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'verifyToken' => Str::random(40),
         ]);
     }
 }
