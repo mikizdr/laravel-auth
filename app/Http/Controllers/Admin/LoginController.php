b77626af44139b1627cbd9874d3d1a55eb5d510c
mikizdr <miroslav.zdravkovic@gmail.com>
2017-05-08 13:34:35 +0200

    Added Illuminate\Http\Request; in the header of LoginController

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
